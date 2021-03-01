72271b87b11155e8c63ab7d604f51cce0fc6fd83
Ishaq Kassam <ishaq.kassam@gmail.com>
2021-03-01 15:56:18 +0300

    established routes for pages

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import { Router } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Chat from './components/Chat';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/room">
+            <Chat/>
+          </Route>
+          <Route path="/">
+            <Login/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
