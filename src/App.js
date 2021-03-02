1b10cdfe61d50e8c43efd585eff2ea4284dd884f
Ishaq Kassam <ishaq.kassam@gmail.com>
2021-03-02 16:45:42 +0300

    added some styling on the container

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Chat from './components/Chat';
 import Login from './components/Login';
+import styled from 'styled-components';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Container>
+        <Header/>
+        
         <Switch>
           <Route path="/room">
             <Chat/>
@@ -16,9 +21,17 @@ function App() {
             <Login/>
           </Route>
         </Switch>
+        </Container>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: blue;
+
+`
